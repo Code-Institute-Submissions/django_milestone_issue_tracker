866e38a5028c09569e1e0b84fe64c57b31ad84a9
Ubuntu <ubuntu@ip-172-31-87-51.ec2.internal>
2019-07-18 09:13:35 +0000

    Adding environment variable for database

--- issueTracker/settings.py
+++ issueTracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 # import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -84,13 +85,16 @@ WSGI_APPLICATION = 'issueTracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+else:
+    print("Database url is not found, default to using sqlite")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
