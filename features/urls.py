71dafb2020914738ef8e82a1c02ca22c8b58d632
Ubuntu <ubuntu@ip-172-31-87-51.ec2.internal>
2019-07-18 17:35:47 +0000

    Add create feature form

--- features/urls.py
+++ features/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import features
+from .views import features, create_feature
 
 urlpatterns = [
     url(r'^all/$', features, name='features'),
+    url(r'^feature-request/$', create_feature, name='create_feature'),
 ]
--- features/views.py
+++ features/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.shortcuts import render, redirect, HttpResponseRedirect, get_object_or_404
 from django.contrib import messages, auth
 from django.template.context_processors import csrf
+from django.contrib.auth.decorators import login_required
+from .models import Feature
+from .forms import FeatureForm
+
 
 
 # Create your views here.
 def features(request):
     """A view that displays the features page"""
-    return render(request, "features.html")
+    features = Feature.objects.all()
+    return render(request, "features.html", {"features": features})
 
+@login_required 
+def create_feature(request, pk=None):
+    """
+    Create a view that allows us to create
+    a bug
+    """
+    feature = get_object_or_404(Feature, pk=pk) if pk else None
+    if request.method == "POST":
+        form = FeatureForm(request.POST, request.FILES, instance=feature)
+        if form.is_valid():
+            feature = form.save(commit=False)
+            featureauthor = request.user.id
+            feature.author_id = featureauthor
+            feature = form.save()
+            return redirect(features)
+    else:
+        form = FeatureForm(instance=feature)
+    return render(request, 'feature_request_form.html', {'form': form})
