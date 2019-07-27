a84ae5e0e319bac22a562ddb918806e45f31286e
Ubuntu <ubuntu@ip-172-31-87-51.ec2.internal>
2019-07-27 10:52:57 +0000

    Search on product name and description

--- searchbugs/views.py
+++ searchbugs/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.db.models import Q
 from bugs.models import Bug
 
 # Create your views here.
 def do_search_bugs(request):
-    bugs = Bug.objects.filter(name__icontains=request.GET['q'])
+    bugs = Bug.objects.filter(Q(name__icontains=request.GET['q']) | Q(description__icontains=request.GET['q']))
     return render(request, "bugs.html", {"bugs": bugs})
--- searchfeatures/views.py
+++ searchfeatures/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.db.models import Q
 from features.models import Feature
 
 # Create your views here.
 def do_search_features(request):
-    features = Feature.objects.filter(name__icontains=request.GET['q'])
+    features = Feature.objects.filter(Q(name__icontains=request.GET['q']) | Q(description__icontains=request.GET['q']))
     return render(request, "features.html", {"features": features})
