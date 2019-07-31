42c1677d2e43fb481bb45c39bf012a2953d6af43
Ubuntu <ubuntu@ip-172-31-87-51.ec2.internal>
2019-07-31 20:01:09 +0000

    PEP8 searchoverview app

--- searchoverview/views.py
+++ searchoverview/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect
-from django.contrib import messages, auth
-from django.template.context_processors import csrf
+from django.shortcuts import render
 from bugs.models import Bug
 from features.models import Feature
 from itertools import chain
 from django.db.models import Q
 
+
 def do_search_overview(request):
-    bugs = Bug.objects.filter(Q(name__icontains=request.GET['q']) | Q(description__icontains=request.GET['q']))
-    features = Feature.objects.filter(Q(name__icontains=request.GET['q']) | Q(description__icontains=request.GET['q']))
-    results = sorted(chain(bugs, features), key=lambda result: result.upvotes, reverse=True)
+    bugs = Bug.objects.filter(
+        Q(name__icontains=request.GET['q']) |
+        Q(description__icontains=request.GET['q'])
+    )
+    features = Feature.objects.filter(
+        Q(name__icontains=request.GET['q']) |
+        Q(description__icontains=request.GET['q'])
+    )
+    results = sorted(chain(bugs, features), key=lambda result: result.upvotes,
+                     reverse=True)
     return render(request, "overview.html", {"results": results})
